4c0da7d691c367c19e265ff90b33e7100ee28f28
Bobthehunter <edu_ph10@hotmail.com>
2017-12-12 22:17:07 +0000

    header completo com todas as cadeiras

--- public_html/templates/header.php
+++ public_html/templates/header.php
@@ -19,73 +19,96 @@
 						  <ul>
 							 <li class='has-sub'><a href='#'><span>1ºAno</span></a>
 								<ul>
-								   <li class="has-sub"><a href='#'><span>1ºSemestre</span></a>
-								   <ul>
-								   <?php $courses=getCourses(1,1);
-					      foreach($courses as $course) {
-					 ?>
-          					<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><span><?php print($course['nome'])?></span></a></li>
-						<?php } ?></ul>
-						</li>
-								   <li class='has-sub'><a href='#'><span>2ºSemestre</span></a>
-								   <ul>
-								   <?php $courses=getCourses(1,2);
-					      foreach($courses as $course) {
-					 ?>
-          					<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a>
-						<?php } ?></ul>
-						</li>
+								<li class="has-sub"><a href='#'><span>1ºSemestre</span></a>
+								<ul>
+								<?php $courses=getCourses(1,1);	foreach($courses as $course) {?>
+          						<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><span><?php print($course['nome'])?></span></a></li>
+								<?php } ?></ul>
+								</li>
+								<li class='has-sub'><a href='#'><span>2ºSemestre</span></a>
+								<ul>
+								<?php $courses=getCourses(1,2);  foreach($courses as $course) {?>
+          						<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a></li>
+								<?php } ?></ul>
+								</li>
 								</ul>
 							 </li>
 
 							 <li class='has-sub'><a href='#'><span>2ºAno</span></a>
 								<ul>
-								   <li><a class='has-sub'><a href='#'><span>1ºSemestre</span></a>
-								   <ul>
-								   <?php $courses=getCourses(2,1);
-					      foreach($courses as $course) {
-					 ?>
-          					<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a>
-						<?php } ?></ul>
-						</li>
-								   <li class='has-sub'><a href='#'><span>2ºSemestre</span></a>
-								   <ul>
-								   <?php $courses=getCourses(2,2);
-					      foreach($courses as $course) {
-					 ?>
-          					<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a>
-						<?php } ?></ul>
-						</li>
+								<li class='has-sub'><a href='#'><span>1ºSemestre</span></a>
+								<ul>
+								<?php $courses=getCourses(2,1); foreach($courses as $course) {?>
+          						<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a></li>
+								<?php } ?></ul>
+								</li>
+								<li class='has-sub'><a href='#'><span>2ºSemestre</span></a>
+								<ul>
+								<?php $courses=getCourses(2,2); foreach($courses as $course) {?>
+          						<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a></li>
+								<?php } ?></ul>
+								</li>
 								</ul>
 							 </li>
 
 							 <li class='has-sub'><a href='#'><span>3ºAno</span></a>
 								<ul>
-								   <li><a href='#'><span>1ºAno</span></a></li>
-								   <li><a href='#'><span>2ºAno</span></a></li>
-								   <li><a href='#'><span>3ºAno</span></a></li>
-								   <li><a href='#'><span>4ºAno</span></a></li>
-								   <li class='last'><a href='#'><span>5ºAno</span></a></li>
+								<?php	$ramos=getRamos(); foreach($ramos as $ramo){?>
+								<li><a href="#"><?php print($ramo['ramo'])?></a>
+								<ul>
+								<?php for($semestre=1 ; $semestre<=2;$semestre++) {?>
+								<li><a href="#"><?php print($semestre)?>ºSemestre</a>
+								<ul>
+								<?php $courses=getCoursesRamo(3,$semestre,$ramo['ramo']); foreach($courses as $course) {?>
+		  						<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a></li>
+								<?php } ?>
+								</ul>
+								</li>
+								<?php } ?>
+								</ul>
+								</li>
+								<?php } ?>
 								</ul>
 							 </li>
 
 							 <li class='has-sub'><a href='#'><span>4ºAno</span></a>
 								<ul>
-								   <li><a href='#'><span>1ºAno</span></a></li>
-								   <li><a href='#'><span>2ºAno</span></a></li>
-								   <li><a href='#'><span>3ºAno</span></a></li>
-								   <li><a href='#'><span>4ºAno</span></a></li>
-								   <li class='last'><a href='#'><span>5ºAno</span></a></li>
-								</ul>
+								   <?php	$ramos=getRamos(); foreach($ramos as $ramo){?>
+									<li><a href="#"><?php print($ramo['ramo'])?></a>
+									<ul>
+									<?php for($semestre=1 ; $semestre<=2;$semestre++) {?>
+									<li><a href="#"><?php print($semestre)?>ºSemestre</a>
+									<ul>
+									<?php $courses=getCoursesRamo(4,$semestre,$ramo['ramo']); foreach($courses as $course) {?>
+		  							<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a></li>
+									<?php } ?>
+									</ul>
+									</li>
+									<?php } ?>
+									</ul>
+									</li>
+									<?php } ?>
+									</ul>
 							 </li>
 
 							 <li class='has-sub'><a href='#'><span>5ºAno</span></a>
 								<ul>
-								   <li><a href='#'><span>1ºAno</span></a></li>
-								   <li><a href='#'><span>2ºAno</span></a></li>
-								   <li><a href='#'><span>3ºAno</span></a></li>
-								   <li><a href='#'><span>4ºAno</span></a></li>
-								   <li class='last'><a href='#'><span>5ºAno</span></a></li>
+								   <?php	$ramos=getRamos(); foreach($ramos as $ramo){?>
+									<li><a href="#"><?php print($ramo['ramo'])?></a>
+									<ul>
+									<?php for($semestre=1 ; $semestre<=2;$semestre++) {?>
+									<li><a href="#"><?php print($semestre)?>ºSemestre</a>
+									<ul>
+									<?php $courses=getCoursesRamo(5,$semestre,$ramo['ramo']); foreach($courses as $course) {?>
+		  							<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a></li>
+									<?php } ?>
+									</ul>
+									</li>
+									<?php } ?>
+									</ul>
+									</li>
+									<?php } ?>
+									</ul>
 								</ul>
 							 </li>
 
@@ -101,66 +124,6 @@
 
 		
 	</div>
-
-		<!--<div class="menu">
-			<ul>
-				<li ><a href="menu.php">Home</a></li>
-
-				<?php for ($year= 1; $year <= 2; $year++) { ?>
-				<li><a href=""><?php print($year) ?>º Ano</a>
-
-				<ul>
-					
-					<?php for($semestre=1 ; $semestre<=2;$semestre++) {?>
-					<li><a href="#"><?php print($semestre)?>ºSemestre</a>
-					<ul>
-					<?php $courses=getCourses($year,$semestre);
-					      foreach($courses as $course) {
-					 ?>
-          					<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a>
-						<?php } ?>
-					</ul>
-					</li>
-					<?php } ?>
-				
-				</ul>
-
-				<?php } ?>
-			
-
-				<?php for ($year= 3; $year <= 5; $year++) { ?>
-				<li><a href=""><?php print($year) ?>º Ano</a>
-				<ul>
-					
-					<?php	$ramos=getRamos();
-						foreach($ramos as $ramo){
-					?>
-					<li><a href="#"><?php print($ramo['ramo'])?></a>
-					<ul>
-						<?php for($semestre=1 ; $semestre<=2;$semestre++) {?>
-						<li><a href="#"><?php print($semestre)?>ºSemestre</a>
-						<ul>
-						<?php $courses=getCoursesRamo($year,$semestre,$ramo['ramo']);
-						      foreach($courses as $course) {
-						 ?>
-		  					<li><a href="course_cats.php?uc_id=<?=$course['id']?>"><?php print($course['nome'])?></a>
-							<?php } ?>
-						</ul>
-						</li>
-						<?php } ?>
-					</ul>
-					</li>
-					<?php } ?>
-
-				</ul>
-				<?php } ?>
-
-				<li style="float:right"><a class="logout" href="action_logout.php">Logout</a></li>
-			</ul>
-			
-		</div>-->
-
-
 
 	 <?php if ($_ERROR_MESSAGE) { ?>
 	    <div class="errors">
